cc7ae13a239a10276942152f10a4dbb92520cac9
niranjanprem <138526507+niranjanprem@users.noreply.github.com>
2023-08-06 16:57:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -11,18 +11,24 @@ let map;
       var d = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat / 2) * Math.sin(difflat / 2) + Math.cos(rlat1) * Math.cos(rlat2) * Math.sin(difflon / 2) * Math.sin(difflon / 2)));
       return d;
     }
+    var my_lat = position.coords.latitude
+    var my_long = position.coords.longitude
+    function initMap() {
 
-    async function initMap() {
-      const position = { lat: 40.7767, lng: -73.9761 };
 
       const { Map } = await google.maps.importLibrary("maps");
       const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
       map = new Map(document.getElementById("map"), {
         zoom: 15,
-        center: position,
+        center: my_lat, my_long,
         mapId: "DEMO_MAP_ID",
       });
+      new google.maps.Marker({
+          position: my_lat, my_long,
+          map,
+          title: "You",
+      })
 
       const marker = new AdvancedMarkerElement({
         map: map,
@@ -30,108 +36,15 @@ let map;
         title: "Uluru",
       });
 
-      var place1 = { lat: 40.7767, lng: -73.9761 };
+      var place1 = { lat: position.coords.latitude, lng: position.coords.longitude};
       var marker1 = new google.maps.Marker({ position: place1, map: map });
 
-      var place2 = { lat: 40.7712, lng: -73.9673 };
+      var place2 = { lat: position.coords.latitude, lng: position.coords.longitude };
       var marker2 = new google.maps.Marker({ position: place2, map: map });
 
       // Calculate and display the distance between markers
       var distance = haversine_distance(marker1, marker2);
-      document.getElementById('msg').innerHTML = "Distance between markers: " + distance.toFixed(2) + " mi.";
+      document.getElementById('msg').innerHTML = "Area Covered: " + (distance.toFixed(2))*0.1 + " mi." + "Percentage Covered:" + (((distance.toFixed(2))*0.1) / 3.55).toFixed(2) + "%" ;
     }
 
     initMap();
-
-
-const startButton = document.getElementById('startButton');
-const stopButton = document.getElementById('stopButton');
-const trashButton = document.getElementById('trashButton');
-
-startButton.addEventListener('click', () => {
-  alert("Miles tracking has started!");
-  InputLocation();
-});
-
-stopButton.addEventListener('click', () => {
-  alert("Miles tracking has stopped.");
-});
-
-trashButton.addEventListener('click', () => {
-  alert("Pinging Trash at your Location.");
-});
-
-function InputLocation() {
-  let myDiv = document.getElementById("GFG");
-            // creating button element
-            let button = document.createElement('BUTTON');
-            // creating text to be
-            //displayed on button
-            let text = document.createTextNode("Button");
-             
-            // appending text to button
-            button.appendChild(text);
-            // appending button to div
-            myDiv.appendChild(button);;
-}
-
-
-function NavBar() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
-
-function createInputField() {
-  // Create an input element
-  var inputField = document.createElement("input");
-  inputField.type = "text";
-  inputField.placeholder = "Enter something";
-
-  // Append the input element to the container
-  var container = document.getElementById("inputFieldContainer");
-  container.appendChild(inputField);
-}
-
-
-
-function showPos(position){   
-  latt = position.coords.latitude;   
-  long = position.coords.longitude;   
-  var lattlong = new google.maps.LatLng(latt, long);   
-  var myOptions = {   
-      center: lattlong,   
-      zoom: 15,   
-      mapTypeControl: true,   
-      navigationControlOptions: {style:google.maps.NavigationControlStyle.SMALL}   
-  }   
-  var maps = new google.maps.Map(document.getElementById("demo"), myOptions);   
-  var markers =   
-  new google.maps.Marker({position:lattlong, map:maps, title:"You are here!"});   
-}   
-
-
-var x= document.getElementById("location");  
-            function getlocation() {  
-                if(navigator.geolocation){  
-                    navigator.geolocation.getCurrentPosition(showPosition)  
-                  }  
-                else  
-                {  
-                     alert("Sorry! your browser is not supporting")  
-                 } }  
-               
-             function showPosition(position){  
-               var y = "Your current location is (" + "Latitude: " + position.coords.latitude + ", " + "Longitude: " +    position.coords.longitude + ")";  
-                        document.getElementById("location").innerHTML = x;  
-             }  
-             function getlocation(){   
-              if(navigator.geolocation){   
-                  navigator.geolocation.getCurrentPosition(showPos, showErr);   
-              }  
-               
-          }  
